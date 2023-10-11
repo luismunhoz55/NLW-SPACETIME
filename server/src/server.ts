98c85d309c1f2d305205d0225482f23e523e942c
luismunhoz55 <munhozluisfernando@gmail.com>
2023-10-11 11:07:28 -0300

    finish github auth

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,8 @@
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import { memoriesRoutes } from './routes/memories'
+import 'dotenv/config'
+import { authRoutes } from './routes/auth'
 
 const app = fastify()
 
@@ -8,6 +10,7 @@ app.register(cors, {
     origin: true, // todas as URLs de frontend poderão acessar nossas rotas backend
 })
 app.register(memoriesRoutes)
+app.register(authRoutes)
 
 app
     .listen({
--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,6 +1,7 @@
-import { User } from 'lucide-react'
-import Image from 'next/image'
-import nlwLogo from './assets/nlw-spacetime-logo.svg'
+import { Copyright } from './components/Copyright'
+import { EmptyMemories } from './components/EmptyMemories'
+import { Hero } from './components/Hero'
+import { Signin } from './components/Signin'
 
 export default function Home() {
   return (
@@ -13,45 +14,18 @@ export default function Home() {
         {/* Stripes */}
         <div className="absolute right-1 top-0 bottom-0 w-2 bg-stripes" />
 
-        {/* Sign in */}
-        <a href="" className="flex items-center gap-3 text-left hover:text-gray-50 transition-colors">
-          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
-            <User className='w-5 h-5 text-gray-500' />
-          </div>
+        <Signin />
 
-          <p className='text-sm leading-snug max-w-[140px]'><span className='underline'>Crie sua conta</span> e salve suas memórias!</p>
-        </a>
+        <Hero />
 
-        {/* Hero */}
-        <div className='space-y-5'>
-          <Image src={nlwLogo} alt="Logo da spaceTime" />
-          <div className='max-w-[420px] space-y-4'>
-            <h1 className='text-5xl font-bold leading-tight text-gray-50'>Sua cápsula do tempo</h1>
-            <p className='text-lg leading-relaxed'>
-              Colecione momentos marcantes da sua jornada e compartilhe (se quiser) com o mundo!
-            </p>
-          </div>
-          <a href="" className='font-bold inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black
-          hover:bg-green-800 transition-colors'
-          >CADASTRAR LEMBRANÇA
-          </a>
-        </div>
-
-        {/* Copyright */}
-        <div className='text-sm leading-relaxed text-gray-200 '>
-          Feito com 💜 no NLW da RocketSeat
-        </div>
+        <Copyright />
+        
 
       </div>
 
       {/* Right */}
       <div className="flex flex-col p-16 bg=[url(../assets/bg-stars.svg)] bg-cover">
-        <div className="flex flex-1 items-center justify-center">
-          <p className="text-center leading-relaxed w-[360px]">
-            Você ainda não registrou nenhuma lembrança, comece a{' '}
-            <a href="" className="underline hover:text-gray-50">criar agora!</a>
-          </p>
-        </div>
+        <EmptyMemories />
       </div>
     </main>
   )
